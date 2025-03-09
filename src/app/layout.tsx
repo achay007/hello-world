e5a9e18ef11ffdeb0805b4742dc251108197a3fb
achay007 <akshayachz09@gmail.com>
2025-03-09 18:32:02 +0530

    setting up next js project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +26,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`} 
       >
-        hh
         {children}
       </body>
     </html>
